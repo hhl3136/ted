bb43280981f0c3a465b7d2c45f16c51081ea74e0
hhl3136 <home3136@163.com>
2016-12-18 11:11:20 +0800

    v1.6

---
+++ src/controller/Manager.java
@@ -0,0 +1,43 @@
+package controller;
+
+
+
+public class Manager {
+	
+	private int id;
+	private String username;
+	private String password;
+	
+	
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public Manager() {
+	}
+	public Manager(String username, String password) {
+		super();
+		this.username = username;
+		this.password = password;
+	}
+
+	
+}
